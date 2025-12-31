73a6accbff50d513a1906bd2c663c195687d78a6
Roman Chursanov <roman@chrsnv.ru>
2025-12-31 16:04:30 +0500

    fix: Импорт клиента

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,8 @@
 
 from .auth import authenticate, get_csrf
 from .config import Settings
+from .client import Client
 
-__all__ = [
-    "authenticate",
-    "get_csrf",
-    "Settings",
-]
+__all__ = ["authenticate", "get_csrf", "Settings", "Client"]
 
 __version__ = "0.0.1"
